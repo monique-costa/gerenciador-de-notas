3b58930d35676decfea898ad6cd03f96a44aefff
Morgana <costanicce@gmail.com>
2021-09-14 21:24:28 -0300

    Updates

---
+++ precache-manifest.f3b8a03ba5bb83e6c734059b3a1e3790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded3f9438f9a8afc10cc8d2f35e5e01a",
+    "url": "/gerenciador-de-notas/index.html"
+  },
+  {
+    "revision": "267d19b5273001da63e4",
+    "url": "/gerenciador-de-notas/static/css/main.4a15dfff.chunk.css"
+  },
+  {
+    "revision": "3d89fbc86edb9d7f0288",
+    "url": "/gerenciador-de-notas/static/js/2.46642ead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gerenciador-de-notas/static/js/2.46642ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267d19b5273001da63e4",
+    "url": "/gerenciador-de-notas/static/js/main.d329c76f.chunk.js"
+  },
+  {
+    "revision": "e7fd7aca9b8871b45e50",
+    "url": "/gerenciador-de-notas/static/js/runtime-main.bdface1b.js"
+  }
+]);
